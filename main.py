b03bdbfa330f6e5e4de5a9dfa9b4e813a994a380
Glyson <kauaglyson@gmail.com>
2022-12-03 15:24:27 -0300

    endpoint que busca dados de um formulário e adiciona ao banco

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from app import app
-from flask import render_template
+from app.models import User
+from flask import render_template, request
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("site.html")
+    return render_template("teste.html")
 
-
+@app.route("/", methods=["POST"])
+def cadastro():
+    CPF = request.form['CPF']
+    Nome = request.form['Nome']
+    Sobrenome = request.form['Sobrenome']
+    Email = request.form['Email']
+    Telefone = request.form['Telefone']
+    Senha = request.form['Senha']
+    Usuario = User(CPF, Nome, Sobrenome, Email, Telefone, Senha)
+    Usuario.adicionarBanco()
+    
+    return "CADASTRADO!!!"
 
 app.run(debug=True)
