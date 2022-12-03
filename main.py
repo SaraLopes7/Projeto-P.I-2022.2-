2ba5425527c5b86718b02a910645728dceef6dc6
Glyson <kauaglyson@gmail.com>
2022-12-03 19:55:36 -0300

    endpoint para login e logout do usuário

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from app import app
 from app.models import User
-from flask import render_template, request
+from flask_login import login_user, logout_user
+from flask import render_template, request, redirect, url_for, flash
 
 
 @app.route("/", methods=["GET"])
 def home():
-    return "home"
+    return render_template("testeHome.html")
 
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
@@ -18,8 +19,30 @@ def cadastro():
         Senha = request.form['Senha']
         Usuario = User(CPF, Nome, Sobrenome, Email, Telefone, Senha)
         Usuario.adicionarBanco()
-        return "CADASTRADO!!!"
-        
+
+        return redirect(url_for('login'))
+
     return render_template("teste.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        Email = request.form['EmailLogin']
+        Senha = request.form['SenhaLogin']
+
+        if not User.verificarEmail(Email) or not User.validarSenha(Email, Senha):
+            return redirect(url_for('login'))
+        else:
+            DadosUsuario = User.buscarUsuarioPorEmail(Email)
+            Usuario = User(DadosUsuario[0], DadosUsuario[1], DadosUsuario[2], DadosUsuario[3], DadosUsuario[4], DadosUsuario[5])
+            login_user(Usuario)
+            return redirect(url_for('home'))
+
+    return render_template("testeLogin.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 app.run(debug=True)
