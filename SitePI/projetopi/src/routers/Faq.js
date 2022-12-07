aa30f3e31588afc01b523a16855cd62490f37831
Filipe Lacerda <gdfkaiser@gmail.com>
2022-12-07 10:27:26 -0300

    FAQ

---
+++ SitePI/projetopi/src/routers/Faq.js
@@ -0,0 +1,34 @@
+import Iframe from 'react-iframe'
+import './Faq.css'
+
+function Faq () {
+    return (
+    <div> 
+        <div id='FAQ'>
+            <div id="linha6">
+                <div id="texto9">
+                    <h2>Pergunta Lorem</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repellendus aspernatur nostrum veritatis fugit autem laborum quasi consectetur blanditiis? Corporis id dolore quibusdam debitis similique velit eligendi aperiam natus repellat.</p>
+                </div>
+                <div id="texto10">
+                    <h2>Pergunta Lorem</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repellendus aspernatur nostrum veritatis fugit autem laborum quasi consectetur blanditiis? Corporis id dolore quibusdam debitis similique velit eligendi aperiam natus repellat.</p>
+                </div>
+            </div>
+            <div id="linha7">
+                <div id="texto11">
+                    <h2>Pergunta Lorem</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repellendus aspernatur nostrum veritatis fugit autem laborum quasi consectetur blanditiis? Corporis id dolore quibusdam debitis similique velit eligendi aperiam natus repellat.</p>
+                </div>
+                <div id="texto12">
+                    <h2>Pergunta Lorem</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis repellendus aspernatur nostrum veritatis fugit autem laborum quasi consectetur blanditiis? Corporis id dolore quibusdam debitis similique velit eligendi aperiam natus repellat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+    )
+}
+
+export default Faq;
